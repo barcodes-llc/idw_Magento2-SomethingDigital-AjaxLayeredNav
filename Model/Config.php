1c038ff7210dd7eb4651ea48a022c7b9faf7d556
Alex Borisenko <aborisenko@somethingdigital.com>
2017-03-01 21:43:58 +0200

    Add enable/disable for multiselect layered navigation

--- Model/Config.php
+++ Model/Config.php
@@ -4,9 +4,30 @@ namespace SomethingDigital\AjaxLayeredNav\Model;
 
 class Config implements ConfigInterface
 {
+    const XML_PATH_ENABLED = 'catalog/layered_navigation/enable_multiselect_layer';
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Check whether entire module functionality is enabled
+     *
+     * @return bool
+     */
     public function enabled()
     {
-        // TODO
-        return true;
+        return $this->scopeConfig->isSetFlag(static::XML_PATH_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 }
