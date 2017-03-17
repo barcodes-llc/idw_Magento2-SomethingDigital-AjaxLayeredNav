1d92d92de6f99f6f2f5f50d19a8d31541e72a93d
Alex Borisenko <aborisenko@somethingdigital.com>
2017-03-17 07:22:18 +0200

    Override Magento\Swatches\Model\Plugin\FilterRenderer

---
+++ Model/Plugin/Swatches/FilterRendererPlugin.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SomethingDigital\AjaxLayeredNav\Model\Plugin\Swatches;
+
+use Magento\Swatches\Model\Plugin\FilterRenderer as FilterRendererBase;
+
+class FilterRendererPlugin extends FilterRendererBase
+{
+}
